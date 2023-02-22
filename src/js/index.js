630b7c622fc1c372d2506d8939855ed85db6c0a9
Daniel Jiménez <djimenezweb@gmail.com>
2023-02-22 16:28:45 +0100

    updated template

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+import CatImage from '../assets/images/anubis.jpg';
 import { sayHello } from './demo.js';
 
 sayHello();
+
+const img = document.createElement('img');
+img.src = CatImage;
+document.body.append(img);
